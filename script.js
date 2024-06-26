14404e4f393aa871379fdca6a2ed57b1ef79221c
MemoDamus <gvmemo.93@gmail.com>
2024-06-26 13:42:03 -0600

    Se aplico la paleta de colores y se hizo el grid del template, se acomodaron los datos en cada uno de sus paneles.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const result = document.querySelector('.result');
+const currentWeather = document.querySelector('.current-weather');
 const form = document.querySelector('.weather');
 const nameCity = document.querySelector('#city');
 const nameCountry = document.querySelector('#country');
-const prueba = document.querySelector('.prueba')
+const weatherTime = document.querySelector('.weather-time')
+const sectionFeelsLike = document.querySelector('#section-feels-like');
+const sectionWindSpeed = document.querySelector('#section-wind-speed');
+const sectionSunset = document.querySelector('#section-sunset');
+const sectionHumidity = document.querySelector('#section-humidity');
 
 
 form.addEventListener('submit', (e)=>{
@@ -16,7 +20,7 @@ form.addEventListener('submit', (e)=>{
 })
 
 const callAPI=(city, country)=>{
-    const apiId = '7fd9ba295d2228d7c60ae6dedc1375f9';
+    const apiId = '';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
     fetch(url)
         .then(response => response.json())
@@ -45,42 +49,40 @@ const callAPI=(city, country)=>{
         })
 }
 
-const showWeatherTime =(data)=>{
-    for(let i=0; i<8; i++){
-        const arr = data.list[i];
-        const {temp} = arr.main;
-        const [{main, description, icon}] = arr.weather;
-        console.log(`Temp: ${temp}`);
-        console.log(main, description, icon)
-        const degrees = kelvinToCentigradetemp(temp)
-        const content = document.createElement('section');
-        content.classList.add =('section-data-weather')
-        content.innerHTML = `
-            <img src='https://openweathermap.org/img/wn/${icon}@2x.png' alt'Weather Icon'>
-            <p>${main}, ${description}</p>
-            <h2>${degrees}°C</h2>`
-        prueba.appendChild(content);
-    }
-}
-
 const showWeather=(data)=>{
     const {name, main:{temp,temp_min,temp_max, feels_like, humidity}, weather:[arr], visibility, wind:{speed}, sys:{sunrise, sunset}} = data;
     const degrees = kelvinToCentigradetemp(temp);
     const minDegrees = kelvinToCentigradetemp(temp_min);
     const maxDegrees = kelvinToCentigradetemp(temp_max);
-    console.log(feels_like, humidity, visibility, speed, sunrise, sunset)
-    const content = document.createElement('section');
-    content.classList.add =('section-data-weather')
+    const content = document.createElement('article');
+    content.classList.add('section-data-weather')
     content.innerHTML = `
-            <h3>Clima en ${name}</h2>
+            <h3>${name}</h3>
             <img src='https://openweathermap.org/img/wn/${arr.icon}@2x.png' alt'Weather Icon'>
-            <p>${arr.main}, ${arr.description}</p>
-            <h2>${degrees}°C</h2>
-            <p>Max: ${maxDegrees}°C</p>
-            <p>Min: ${minDegrees}°C</p>`
-    result.appendChild(content);
+            <h2>${degrees}°</h2>
+            <p>${arr.description}</p>
+            <p>Maxima: ${maxDegrees}°  Minima: ${minDegrees}°</p>`
+    currentWeather.appendChild(content);
+    printFeelsLike(feels_like, sectionFeelsLike, 'feels-like');
+    printHumidity(humidity, sectionHumidity, 'feels-like');
+    printSpeedWind(speed, sectionWindSpeed, 'feels-like');
+    printSunset(sunset, sectionSunset, 'feels-like');
 }
-
+const showWeatherTime =(data)=>{
+    for(let i=0; i<8; i++){
+        const arr = data.list[i];
+        const {temp} = arr.main;
+        const [{main, description, icon}] = arr.weather;
+        const degrees = kelvinToCentigradetemp(temp)
+        const content = document.createElement('article');
+        content.classList.add('box-daily-weather')
+        content.innerHTML = `
+            <img src='https://openweathermap.org/img/wn/${icon}@2x.png' alt'Weather Icon'>
+            <p>${description}</p>
+            <h2>${degrees}°</h2>`
+        weatherTime.appendChild(content);
+    }
+}
 const showError=(message)=>{
     console.log(message);
     const alert = document.createElement('p');
@@ -92,8 +94,50 @@ const showError=(message)=>{
     }, 1000);
 }
 const clearHTML =()=>{
-    result.innerHTML = ''
+    currentWeather.innerHTML = ''
 }
-
 const kelvinToCentigradetemp=(temp)=>parseInt(temp - 273.15);
 
+const printFeelsLike =(data, section, className)=>{
+    const degrees = kelvinToCentigradetemp(data)
+    const element = document.createElement('article');
+    element.classList.add(className)
+    element.innerHTML = `
+        <h2>Sensacion</h2>
+        <h3>${degrees}°</h3>
+    `
+    section.appendChild(element);
+}
+const printHumidity =(data, section, className)=>{
+    const element = document.createElement('article');
+    element.classList.add(className);
+    element.innerHTML = `
+        <h2>Humedad</h2>
+        <h3>${data}%<h3/>
+    `
+    section.appendChild(element);
+}
+
+const printSpeedWind =(data, section, className)=>{
+    const element = document.createElement('article');
+    const KmS = data*3.6
+    element.classList.add(className);
+    element.innerHTML = `
+        <h2>Viento</h2>
+        <h3>${KmS}Km/h</h3>
+    `
+    section.appendChild(element);
+}
+
+const printSunset=(data, section, className)=>{
+    let time = data*1000;
+    let date = new Date(time);
+    let utcDate = date.toUTCString();
+    const element = document.createElement('article');
+    element.classList.add(className);
+    element.innerHTML = `
+        <h2>Atarceder</h2>
+        <h3>${utcDate}</h3>
+    `
+    section.appendChild(element);
+}   
